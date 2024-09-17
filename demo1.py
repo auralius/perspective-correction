bccb7fc3dcb1c9e793a9c52409dbf0c4b3f09752
auralius manurung <auralius.manurung@formulatrix.com>
2024-09-17 21:46:36 +0700

    Restructure demo1.py.

--- demo1.py
+++ demo1.py
@@ -1,36 +1,38 @@
+import sys
 from corrector import *
 
 
 cap = cv2.VideoCapture('src.mp4') # HD 1920 x 1080 @ 12.5Hz
 
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
+if (cap.isOpened() == False): 
+  print("Error opening video stream or file!")
+  sys.exit()
 
-else:
-  cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
-  cv2.resizeWindow("Video", int(1920/3) , int(1080/3)) 
+print('Running, please wait...')
 
-  q = Corrector()
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Video", int(1920/3) , int(1080/3)) 
 
-  out = cv2.VideoWriter("out.mp4", fourcc=cv2.VideoWriter_fourcc(*'mp4v'), fps=12.5, frameSize=(1920, 1080))
+out = cv2.VideoWriter("out.mp4", fourcc=cv2.VideoWriter_fourcc(*'mp4v'), fps=12.5, frameSize=(1920, 1080))
+q = Corrector()
 
-  while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-      frame = q.prefilter(frame)
-      centers = q.get_image_points(frame, aruco_size=50)
-      frame = q.do_correction(frame, centers)
-      out.write(frame) # Write out frame to video
+while(True):
+  ret, frame = cap.read()
+  
+  if ret == True:
+    frame = q.prefilter(frame)
+    centers = q.get_image_points(frame, aruco_size=50)
+    frame = q.do_correction(frame, centers)
+    out.write(frame) # Write out frame to video
 
-      cv2.imshow('Video',frame)
+    cv2.imshow('Video',frame)
+    cv2.waitKey(1)
 
-      if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+  else: 
+    break
 
-    else: 
-      break
+out.release()
+cap.release()
+cv2.destroyAllWindows()
 
-  out.release()
-  cap.release()
-  cv2.destroyAllWindows()
-
+print("bye...")
